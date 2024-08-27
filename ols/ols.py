e0dbb08a72d1c6d60aee361fe37356e74536bb41
Huynh nt <huynhnt6995@gmail.com>
2024-08-27 17:40:18 +0700

    add ridge

--- ols/ols.py
+++ ols/ols.py
@@ -5,15 +5,13 @@ X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
 y = np.array([6, 8, 9, 11])
 
 # add the initial bias (w0) to each item in X
-(x_row,) = shape_X = X.shape
-init_bias = np.ones((x_row, 1))
+num_of_row = X.shape[0]
+init_bias = np.ones((num_of_row, 1))
 X = np.hstack([init_bias, X])
 
 # calculate W vector following the OLS formula
 XT = X.T
 W = np.linalg.inv(XT @ X) @ XT @ y
-
-print(W)
 
 # predict
 y_pred = X @ W
